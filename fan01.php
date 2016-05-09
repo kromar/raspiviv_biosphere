9820b3f4c0c69d44ca72e26181b77e79356177a1
kromar <grauer.d@gmail.com>
2016-05-10 00:09:39 +0200

    inverted rain trigger; added windTime

--- fan01.php
+++ fan01.php
@@ -1,64 +1,69 @@
-<?php 
+<?php
 
-$db = mysql_connect("localhost","datalogger","datalogger") or die("DB Connect error"); 
-mysql_select_db("datalogger"); 
+	$db = mysql_connect("localhost","datalogger","datalogger") or die("DB Connect error");
+	mysql_select_db("datalogger");
 
-$q = "SELECT humidity FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1"; 
-$ds = mysql_query($q); 
-$humiditySensor=(int)mysql_fetch_object($ds)->humidity; 
-
-
-/* used to execute a python script
- * $command = escapeshellcmd(' /usr/custom/test.py');
-* $output = shell_exec($command);
-* echo $output
-*/
-
-// grap weather information from openweathermap.org
-$city = "Chepo";
-$country = "PA"; // two digit country code
-
-$url = "http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
-$json = file_get_contents($url);
-$data = json_decode($json,true);
-//get humidity
-echo $data['main']['humidity']."<br>";
+	$q = "SELECT humidity FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
+	$ds = mysql_query($q);
+	$humiditySensor=(int)mysql_fetch_object($ds)->humidity;
 
 
-//change threshold depening on time of day
-$humidityThreshold;
-$humidityNight = 85.0;
-$humidityDay = 95.0;
+	/* used to execute a python script
+	 * $command = escapeshellcmd(' /usr/custom/test.py');
+	* $output = shell_exec($command);
+	* echo $output
+	*/
 
-$t = time();
-$curentTime = date('H:i');
-$morningTime = ('08:00');
-$eveningTime = ('22:00');
+	// grap weather information from openweathermap.org
+	$city = "Chepo";
+	$country = "PA"; // two digit country code
 
-if (($curentTime < $morningTime) or ($curentTime > $eveningTime))
-{
-	$humidityThreshold = $humidityNight;
-}	
-else
-{
-	$humidityThreshold = $humidityDay;
-} 
+	$url = "http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
+	$json = file_get_contents($url);
+	$data = json_decode($json,true);
+	//get humidity
+	echo $data['main']['humidity']."<br>";
 
 
-//change power state of fan depending on current humidity
-if ($humiditySensor > $humidityThreshold) 
-{ 
-	$pwmNew=1; 
-} 
-if ($humiditySensor <= $humidityThreshold) 
-{ 
-	$pwmNew=0; 
-} 
+	//change threshold depening on time of day
+	$humidityThreshold;
+	$humidityNight = 85.0;
+	$humidityDay = 95.0;
+	$windTime = 10;
+
+	$t = time();
+	$curentTime = date('H:i');
+	$morningTime = ('08:00');
+	$eveningTime = ('22:00');
+
+	//set day or nighttime humidity
+	if (($curentTime < $morningTime) or ($curentTime > $eveningTime))
+		{
+			$humidityThreshold = $humidityNight;
+		}
+		else
+		{
+			$humidityThreshold = $humidityDay;
+		}
 
 
-$s="/usr/local/bin/gpio write 5 $pwmNew "; 
-exec($s); 
+	//change power state of fan depending on current humidity
+	if ($humiditySensor <= $humidityThreshold)
+		{
+			//let it wind
+			exec('/usr/local/bin/gpio mode 5 out');
+			exec('/usr/local/bin/gpio write 5 1');
 
-mysql_query($q); 
-mysql_close($db); 
-?> 
+			//time till wind stops
+			sleep ($windTime);
+			exec('/usr/local/bin/gpio mode 5 out');
+			exec('/usr/local/bin/gpio write 5 0');
+		}
+
+
+	//$s="/usr/local/bin/gpio write 5 $pwmNew ";
+	//exec($s);
+
+	mysql_query($q);
+	mysql_close($db);
+?>
--- rain01.php
+++ rain01.php
@@ -1,74 +1,69 @@
 <?php
 
-$db = mysql_connect("localhost","datalogger","datalogger") or die("DB Connect error");
-mysql_select_db("datalogger");
+	$db = mysql_connect("localhost","datalogger","datalogger") or die("DB Connect error");
+	mysql_select_db("datalogger");
 
-$q = "SELECT temperature FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
-$ds = mysql_query($q);
-$tempSensor=(int)mysql_fetch_object($ds)->temperature;
-
-
-/* used to execute a python script
- * $command = escapeshellcmd(' /usr/custom/test.py');
- * $output = shell_exec($command);
- * echo $output
- */
-
-// grab weather information from openweathermap.org
-$city = "Chepo";
-$country = "PA"; // two digit country code
-
-$url = "http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
-$json = file_get_contents($url);
-$data = json_decode($json,true);
-//get rmep in celcius
-echo $data['main']['temp']."<br>";
+	$q = "SELECT temperature FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
+	$ds = mysql_query($q);
+	$tempSensor=(int)mysql_fetch_object($ds)->temperature;
 
 
-//change threshold depening on time of day
-$tempThreshold;
-$tempNight = 24.5;
-$tempDay = 28.5;
+	/* used to execute a python script
+	 * $command = escapeshellcmd(' /usr/custom/test.py');
+	 * $output = shell_exec($command);
+	 * echo $output
+	 */
 
-$rainTime = 1; //time in seconds to rain
+	// grab weather information from openweathermap.org
+	$city = "Chepo";
+	$country = "PA"; // two digit country code
 
-$t = time();
-$curentTime = date('H:i');
-$morningTime = ('08:00');
-$eveningTime = ('22:00');
-
-if (($curentTime < $morningTime) or ($curentTime > $eveningTime))
-{
-	$tempThreshold = $tempNight;
-}
-else
-{
-	$tempThreshold = $tempDay;
-}
+	$url = "http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
+	$json = file_get_contents($url);
+	$data = json_decode($json,true);
+	//get rmep in celcius
+	echo $data['main']['temp']."<br>";
 
 
+	//change threshold depening on time of day
+	$tempThreshold;
+	$tempNight = 24.5;
+	$tempDay = 28.5;
 
-if ($tempSensor > $tempThreshold)
-{
-	//$pwmNew=1;
-	//let it rain
-	exec('/usr/local/bin/gpio mode 2 out');
-	exec('/usr/local/bin/gpio write 2 1');
+	$rainTime = 10; //time in seconds to rain
 
-	//time till rain stops
-	sleep ($rainTime);
-	exec('/usr/local/bin/gpio mode 2 out');
-	exec('/usr/local/bin/gpio write 2 0');
-}
+	$t = time();
+	$curentTime = date('H:i');
+	$morningTime = ('08:00');
+	$eveningTime = ('22:00');
+
+	//set day or nighttime temp
+	if (($curentTime < $morningTime) or ($curentTime > $eveningTime))
+		{
+			$tempThreshold = $tempNight;
+		}
+		else
+		{
+			$tempThreshold = $tempDay;
+		}
 
 
+	//react to sensor temperatures
+	if ($tempSensor > $tempThreshold)
+		{
+			//let it rain
+			exec('/usr/local/bin/gpio mode 2 out');
+			exec('/usr/local/bin/gpio write 2 0');
+
+			//time till rain stops
+			sleep ($rainTime);
+			exec('/usr/local/bin/gpio mode 2 out');
+			exec('/usr/local/bin/gpio write 2 1');
+		}
 
 
-//$s="/usr/local/bin/gpio write 2 $pwmNew ";
-//exec($s);
-
-mysql_query($q);
-mysql_close($db);
+	mysql_query($q);
+	mysql_close($db);
 ?>
 
 
