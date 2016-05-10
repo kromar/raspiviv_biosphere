52a8e4c42effa48a37097cf1f93b6dfaa42026d1
kromar <grauer.d@gmail.com>
2016-05-10 22:06:54 +0200

    changed layout; removed gauge test file

--- graph03.php
+++ graph03.php
@@ -196,10 +196,16 @@ if(!$fgmembersite->CheckLogin())
 	</div>
 	
 	<div class="container">		
-		<div class="row">			
-			<div id="temp_gauge_div" style="width: auto; height: auto;"></div>
-		    <div id="hum_gauge_div" style="width: auto; height: auto;"></div>
-			<div id="chart_div" style="width: auto; height: auto;"/>
+		<div class="row">
+			<div class="col-sm-3">
+				<div id="temp_gauge_div" style="width: auto; height: auto;"/>
+			    <div id="hum_gauge_div" style="width: auto; height: auto;"/>
+			</div>
+			<div class="col-sm-9">
+    			<div id="chart_div" style="width: auto; height: auto;"/>
+			</div>
+		</div>		
+		
 	    <hr>
 	    <?php include 'footer.php';?>
 	</div>
