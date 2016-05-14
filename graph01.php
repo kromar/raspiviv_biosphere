db3b79db9fea3eb72b693eb56e32e1c804ed8661
kromar <grauer.d@gmail.com>
2016-05-15 01:21:00 +0200

    print url

--- graph01.php
+++ graph01.php
@@ -7,9 +7,6 @@
 	    exit;
 	}
 
-
-
-
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -35,7 +32,7 @@
 			$data = json_decode($json,true);
 			//get humidity
 			$humidityOWM =  $data['main']['humidity']."<br>";
-			echo ($humidityOWM);
+			echo ($url);
 			//echo("<script>console.log('test');</script>");
 			echo("<script>console.log($humidityOWM);  </script>");
 		?>
