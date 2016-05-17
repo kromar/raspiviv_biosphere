654257c4c4c73f4d27cc3d4cf2b683c5d3256d22
kromar <grauer.d@gmail.com>
2016-05-17 18:56:53 +0200

    test output

--- graph01.php
+++ graph01.php
@@ -404,13 +404,13 @@
 			$db = mysql_connect("localhost","datalogger","datalogger") or die("DB Connect error");
 			mysql_select_db("datalogger");
 
-			$qt = "SELECT temperature FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
+			$qt = "SELECT * FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
 			$dt = mysql_query($qt);
 			$tempSensor=(float)mysql_fetch_object($dt)->temperature;
 
-			$qh = "SELECT humidity FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
-			$dh = mysql_query($qh);
-			$humSensor=(float)mysql_fetch_object($dh)->humidity;
+			//$qh = "SELECT humidity FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
+			//$dh = mysql_query($qh);
+			$humSensor=(float)mysql_fetch_object($dt)->humidity;
 
 			var_dump("<script>console.log('$tempSensor');</script>", false);
 			var_dump("<script>console.log('$humSensor');</script>", false);
