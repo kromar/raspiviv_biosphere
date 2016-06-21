eee8e0774ed3765d4d24e25ef439ac95fba9760a
kromar <grauer.d@gmail.com>
2016-06-21 22:59:21 +0200

    new test

--- climate_control.php
+++ climate_control.php
@@ -10,19 +10,19 @@
 
 	$qh = "SELECT humidity FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
 	$dh = mysql_query($qh);
-	$humiditySensor=mysql_fetch_object($dh)->humidity;
+	$humiditySensor=(float)mysql_fetch_object($dh)->humidity;
 
 	logToFile("hum, temp", ($humiditySensor."-".$tempSensor));
 
-	/*
+	//*
 	$test = "SELECT humidity,temperature FROM datalogger where sensor = 8 ORDER BY date_time DESC LIMIT 1";
 	while ($row = (float)mysql_fetch_obejct($test)) {
 		$h = $row->humidity;
 		$t = $row->temperature;
 	}
-	*/
+	logToFile("hum, temp", ($h."-".$t));
+	//*/
 
-	//logToFile("hum, temp", ($h."-".$t));
 
 
 	//change threshold depening on time of day
@@ -144,9 +144,9 @@
 		exec('/usr/local/bin/gpio write 5 0');
 	}
 
-	//logToFile("", ""); //log time if nothing else happens
+	logToFile(); //log time if nothing else happens
 	function logToFile($string, $value) {
-		$mylogfile = fopen(__DIR__ . "/../debug.log", "w") or die("Unable to open file!");
+		$mylogfile = fopen(__DIR__ . "/../debug.log", "a") or die("Unable to open file!");
 		$fileSize = fgets($mylogfile);
 		$curentTime = date('H:i:s');
 		fwrite($mylogfile, $curentTime." filesize: ".$fileSize. "\n");
