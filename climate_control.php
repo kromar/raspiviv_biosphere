e3c882deab9d2711e687837a03e1fdf4836ba06d
kromar <grauer.d@gmail.com>
2016-06-22 23:47:02 +0200

    changing file path

--- climate_control.php
+++ climate_control.php
@@ -149,13 +149,13 @@
 	}
 
 	function logToFile($string, $value) {
-		$file = "./../debug.log";
-		$size = filesize("debug.log");
+		$file = dirname(__FILE__) . "/../debug.log";
+		$size = filesize(dirname(__FILE__) . "/../" . "debug.log");
 		if (filesize < 1024) {
 			$mylogfile = fopen(__DIR__ . $file, "a") or die("Unable to open file!");
 			$curentTime = date('H:i:s');
 			try {
-				fwrite($mylogfile, $curentTime . " size: " . $size . "bytes\n " ."file: ". $file ."\ndir: ". dirname(__FILE__) . "\n");
+				fwrite($mylogfile, $curentTime . " size: " . $size . "bytes\n" ."file: ". $file ."\ndir: ". dirname(__FILE__) . "\n");
 				fwrite($mylogfile, $curentTime . "  " . $string . ": " . $value . "\n");
 				fclose($mylogfile);
 
