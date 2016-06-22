bd1d0c55fcac0d99d3590a817b63fd62b1840106
kromar <grauer.d@gmail.com>
2016-06-22 14:05:42 +0200

    fixed fetch

--- climate_control.php
+++ climate_control.php
@@ -21,7 +21,7 @@
 	//now we need to get those 2 row values
 	if (mysql_num_rows($result) > 0) {
 		// output data of each row
-		while($row = mysqli_fetch_assoc($result)) {
+		 while($row = $result->fetch_assoc()) {
 			logToFile("ht", "humid: " . $row["humidity"]. " - temp: " . $row["temperature"]);
 		}
 	} else {
