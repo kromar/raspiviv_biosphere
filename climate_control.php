1c63d2de6889cc1f2dcec671175c811f07c9f251
kromar <grauer.d@gmail.com>
2016-06-23 00:05:25 +0200

    test fixing log

--- climate_control.php
+++ climate_control.php
@@ -150,7 +150,7 @@
 
 	function logToFile($string, $value) {
 		$file = "../debug.log";
-		$size = filesize(__DIR__ . $file);
+		$size = filesize($file);
 		if (1024 == 1024) {
 			$mylogfile = fopen(__DIR__ . $file, "a") or die("Unable to open file!");
 			$curentTime = date('H:i:s');
