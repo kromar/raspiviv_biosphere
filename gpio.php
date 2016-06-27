da1d3abc3aed1c5877995bc9111db3bafe9b2396
kromar <grauer.d@gmail.com>
2016-06-27 18:24:39 +0200

    uncomment logging

--- gpio.php
+++ gpio.php
@@ -1,6 +1,6 @@
 ﻿<!-- This page is requested by the JavaScript, it updates the pin's status and then print it -->
 <?php
-	//include_once 'log.php';
+	include 'log.php';
 	//*
 	//Getting and using values
 	if (isset ($_GET["pin"]) && isset($_GET["status"]) ) {
@@ -10,7 +10,7 @@
 		//Testing if values are numbers
 		if ( (is_numeric($pin)) && (is_numeric($status)) && ($pin <= 7) && ($pin >= 0) && ($status == "0") || ($status == "1") ) {
 
-			//logToFile("testing trigger", 1,1);
+			logToFile("testing trigger", 1,1);
 
 			//set the gpio's mode to output
 			system("gpio mode ".$pin." out");
@@ -25,10 +25,10 @@
 			//reading pin's status
 			exec ("gpio read ".$pin, $status, $return );
 			//printing it
-			//logToFile( "status", $pin, $status[0] );
+			logToFile( "status", $pin, $status[0] );
 		}
-		else { //logToFile("fail"); }
+		else { logToFile("fail"); }
 	} //print fail if cannot use values
-	else { //logToFile("fail"); }
+	else { logToFile("fail"); }
 	//*/
 ?>
