ddd416e6594dc98805490bf459b6a5760d2dfaf0
kromar <grauer.d@gmail.com>
2016-06-27 14:30:23 +0200

    added logging to buttons file

--- buttons.php
+++ buttons.php
@@ -32,33 +32,23 @@
 		<div class="container">
 		     <!-- On/Off button's picture -->
 			 <?php
+			 	include('log.php');
 				 //this php script generate the first page in function of the gpio's status
 				 $status = array(0, 0, 0, 0, 0, 0, 0);
 
-				 for ($i = 0; $i < count($status); $i++) {
+				 for ($pin = 0; $pin < count($status); $pin++) {
 					//set the pin's mode to output and read them
-					system("gpio mode ".$i." out");
-					exec ("gpio read ".$i, $status[$i], $return );
+					system("gpio mode ".$pin." out");
+					exec ("gpio read ".$pin, $status[$pin], $return );
+					logToFile("gpio read", $pin, $status);
 
-					//inverse pins 2 & 3 because they have pull high on boot
-					if ($status[$i] == 2 or $status[$i] == 3) {
-						// if off
-						if ($status[$i][0] == 1 ) {
-							echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".png' alt='off'/><br>");
-						}
-						// if on
-						if ($status[$i][0] == 0 ) {
-							echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".png' alt='on'/><br>");
-						}
-					} else {
-						// if off
-						if ($status[$i][0] == 0 ) {
-							echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".png' alt='off'/><br>");
-						}
-						//if on
-						if ($status[$i][0] == 1 ) {
-							echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".png' alt='on'/><br>");
-						}
+					// if off
+					if ($status[$pin][0] == 0 ) {
+						echo ("<img id='button_".$pin."' src='data/img/red/red_".$pin.".png' alt='off'/><br>");
+					}
+					//if on
+					if ($status[$pin][0] == 1 ) {
+						echo ("<img id='button_".$pin."' src='data/img/green/green_".$pin.".png' alt='on'/><br>");
 					}
 				 }
 			 ?>
