4f468df8df349b8636b4aeaa1a4589b31abfef35
kromar <grauer.d@gmail.com>
2016-07-03 14:33:06 +0200

    removed double head from buttons; added favicons

--- buttons.php
+++ buttons.php
@@ -10,8 +10,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <html>
-  <head>
-<html>
 	<head>
 	<title>RasPiViv.com - Buttons</title>
 		<script src="scripts/jquery-1.12.3.min.js"></script>
--- index.php
+++ index.php
@@ -11,6 +11,26 @@ if(!$fgmembersite->CheckLogin())
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
+
+	<link rel="apple-touch-icon" sizes="57x57" href="/data/img/icon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/data/img/icon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/data/img/icon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/data/img/icon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/data/img/icon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/data/img/icon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/data/img/icon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/data/img/icon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/data/img/icon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/data/img/icon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/data/img/icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/data/img/icon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/data/img/icon/favicon-16x16.png">
+	<link rel="manifest" href="/data/img/icon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/data/img/icon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+
+
 <title>RasPiViv.com - Home</title>
 	<script src="scripts/jquery-1.12.3.min.js"></script>
 	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
