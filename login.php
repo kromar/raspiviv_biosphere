db946c67f0b7e6b97f51d18c96415105f373dd9d
kromar <grauer.d@gmail.com>
2016-07-03 18:17:10 +0200

    bughunt

--- login.php
+++ login.php
@@ -20,15 +20,15 @@ if(isset($_POST['submitted']))
     <meta name="author" content=""/>
 
     <title>RasPiViv - Sign in</title>
-	<script src="scripts/jquery-1.12.3.min.js"/>
-	<script type="text/javascript" src="https://www.google.com/jsapi"/>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"/>
-    <script type='text/javascript' src='scripts/gen_validatorv31.js'/>
+	<script src="scripts/jquery-1.12.3.min.js"</script>
+	<script type="text/javascript" src="https://www.google.com/jsapi"</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"</script>
+    <script type='text/javascript' src='scripts/gen_validatorv31.js'</script>
 
-    <link href="/css/signin.css" rel="stylesheet"/>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
-   	<link rel="stylesheet" href="http://bootswatch.com/cerulean/bootstrap.min.css"/>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"/>
+    <link href="/css/signin.css" rel="stylesheet">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+   	<link rel="stylesheet" href="http://bootswatch.com/cerulean/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
