7ab913a0e926da4a70ccee6b154c61b004dafd5d
kromar <grauer.d@gmail.com>
2016-07-03 18:04:47 +0200

    debugging

--- graph01.php
+++ graph01.php
@@ -13,9 +13,9 @@
 <head>
 	<title>RasPiViv.com - Vivarium 1</title>
 
-	<script src="scripts/jquery-1.12.3.min.js"/>
-	<script type="text/javascript" src="https://www.google.com/jsapi"/>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"/>
+	<script src="scripts/jquery-1.12.3.min.js"</script>
+	<script type="text/javascript" src="https://www.google.com/jsapi"</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"</script>
 
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
    	<link rel="stylesheet" href="http://bootswatch.com/cerulean/bootstrap.min.css"/>
