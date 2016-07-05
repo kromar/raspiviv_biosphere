aa71ae8d0442a513ce4fa768b4401a5100b16f5f
kromar <grauer.d@gmail.com>
2016-07-05 11:35:52 +0200

    testing escape

--- index.php
+++ index.php
@@ -12,14 +12,16 @@
 <head>
 	<title>RasPiViv.com - Home</title>
 
-	<script src="scripts/jquery-1.12.3.min.js"></script>
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script src="scripts/jquery-1.12.3.min.js"/>
+	<script type="text/javascript" src="https://www.google.com/jsapi"/>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"/>
 
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
    	<link rel="stylesheet" href="http://bootswatch.com/cerulean/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
 
+</head>
+	<body>
 	<!-- ROOM TEMP GAUGE -->
 	<script type="text/javascript">
 		google.load("visualization", "1", {packages:["gauge"]});
@@ -365,8 +367,6 @@
 		}
 	</script>
 
-</head>
-	<body>
 		<div class="jumbotron">
 			<div class="container">
 				<?php include 'menu.php';?>
