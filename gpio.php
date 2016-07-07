beb09fce6437ac3170ab4410447e0114dcdee4e3
kromar <grauer.d@gmail.com>
2016-07-07 12:02:21 +0200

    improved buttons log

--- gpio.php
+++ gpio.php
@@ -5,30 +5,35 @@
 	if (isset ($_GET["pin"]) && isset($_GET["status"]) ) {
 		$pin = strip_tags($_GET["pin"]);
 		$status = strip_tags($_GET["status"]);
+		logToFile("define pins", $pin, $status);
 
 		//Testing if values are numbers
-		if ( (is_numeric($pin)) && (is_numeric($status)) && ($pin <= 7) && ($pin >= 0) && ($status == "0") || ($status == "1") ) {
+		if ((is_numeric($pin)) && (is_numeric($status)) && ($pin <= 7) && ($pin >= 0) && ($status == "0") || ($status == "1") ) {
 
 			//set the gpio's mode to output
 			system("gpio mode ".$pin." out");
+			logToFile("set pins", $pin, $status);
+
 			//set the gpio to high/low
 			if ($status == "0" ) {
 				$status = "1";
-				logToFile("set pin", $pin, $status);
+				logToFile("set pin 0-1", $pin, $status);
 			}
 			else if ($status == "1" ) {
 				$status = "0";
-				logToFile("set pin", $pin, $status);
+				logToFile("set pin 1-0", $pin, $status);
 			}
 			system("gpio write $pin $status");
+			logToFile("write pins", $pin, $status);
 
 			//reading pin's status
 			exec ("gpio read ".$pin, $status, $return );
 
 			//printing it
-			logToFile( "status", $pin, $status[0] );
+			logToFile( "pin status", $pin, $status[0] );
 		}
-		else { logToFile("fail 1",'',''); }
+		else { logToFile("fail 1",$pin,$status); }
 	} //print fail if cannot use values
-	else { logToFile("fail 2",'',''); }
+	else { logToFile("fail 2",'','');
+	}
 ?>
