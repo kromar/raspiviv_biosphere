4ae2f7d68b390116323b0cb7554e8e4d4cd6b546
kromar <grauer.d@gmail.com>
2016-07-08 16:23:09 +0200

    check if core is running

--- climate_core.php
+++ climate_core.php
@@ -1,13 +1,22 @@
 <?php
 	include_once 'log.php';
-	$interval = 10;
+	$interval = 30;
 	$somearg;
-	while (true) {
-		//for ($i = 0; $i < $max_clients; $i++)
-		//exec("php sensor.php");
-		//exec("php cliamte_control.php $somearg");	//use ajax so the user wont know the file is loaded
-		logToFile("update interval", $interval, $somearg);
-		sleep($interval);
+	$coreActive = false;
+
+	if ($coreActive == false) {
+		$coreActive = true;
+		logToFile("core activated", '', '');
+		continue;
+	} else {
+		while (true) {
+			//for ($i = 0; $i < $max_clients; $i++)
+			//exec("php sensor.php");
+			//exec("php cliamte_control.php $somearg");	//use ajax so the user wont know the file is loaded
+			logToFile("core interval", $interval, $somearg);
+			sleep($interval);
+		}
+
 	}
 
 	//nohup php myscript.php &		//this creates a service
