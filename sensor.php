be08894916d4e08ccee2292c1d91a560237140fc
kromar <grauer.d@gmail.com>
2016-07-11 17:13:56 +0200

    testing output

--- sensor.php
+++ sensor.php
@@ -7,8 +7,8 @@
 	function readSensor($sensor)
 	{
 		$output = array();
-		exec("sudo loldht $sensor | grep -i 'humidity' | cut -d ' ' -f3", $output);
-		exec("sudo loldht $sensor | grep -i 'temperature' | cut -d ' ' -f7", $output);
+		exec("sudo loldht $sensor | grep -i 'humidity'", $output);
+		//exec("sudo loldht $sensor | grep -i 'temperature' | cut -d ' ' -f7", $output);
 
 		echo "output size: ".count($output)."\n";
 		for ($i=0; $i < count($output); $i++) {
