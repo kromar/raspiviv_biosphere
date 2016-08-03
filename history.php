167bd4d70a52b92bb596a09a39365eb26f19f343
kromar <grauer.d@gmail.com>
2016-08-03 19:20:30 +0200

    fix db parameterss

--- history.php
+++ history.php
@@ -11,8 +11,8 @@
 		if (!$db) {
 			die("Connection failed: " . mysqli_connect_error());
 		}
-		mysqli_select_db("datalogger");
-		$q="delete from datalogger";
+		mysqli_select_db($db, $dbname);
+		$q="delete from $dbname";
 		mysqli_query($db, $q);
 		mysqli_close($db);
 		return 0;
@@ -30,12 +30,12 @@
 		if (!$db) {
 			die("Connection failed: " . mysqli_connect_error());
 		}
-		mysqli_select_db("datalogger");
+		mysqli_select_db($db, $dbname);
 		for ($i=0;$i<=23;$i++)
 		{
 			$q=   "insert into history ( ";
 			$q=$q."select date_add(curdate(),interval $i hour),'$sensor', round(avg(temperature),2),round(avg(humidity),2) ";
-			$q=$q."from datalogger ";
+			$q=$q."from $dbname ";
 			$q=$q."where sensor = '$sensor' ";
 			$q=$q."and date_time >=date_add(curdate(),interval $i hour) "; $ii=$i+1;
 			$q=$q."and date_time < date_add(curdate(),interval $ii hour) ";
