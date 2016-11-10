e22c2ea420c4263853bfee8b46ea52e3433d9c6d
kromar <grauer.d@gmail.com>
2016-11-10 23:29:28 +0100

    updated jquery-3.1.1.min.js; cleaned up head of graph1

--- graph01.php
+++ graph01.php
@@ -10,19 +10,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8"></meta>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 
 	<title>RasPiViv.com - Vivarium 1</title>
 
-
     <!--Load the AJAX API-->
-	<script src="scripts/jquery-1.12.3.min.js"></script>
-	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="scripts/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript"></script>
 
+<!--  load CSS -->
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href="/css/normalize.css" rel="stylesheet"></link>
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"></link>
    	<link rel="stylesheet" href="http://getbootstrap.com/examples/cover/cover.css"></link>
