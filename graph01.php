69fcad4cdfe664037723468ec81ef488865124b9
kromar <grauer.d@gmail.com>
2016-11-14 19:58:25 +0100

    t

--- graph01.php
+++ graph01.php
@@ -65,10 +65,10 @@
 
 				if (mysqli_num_rows($result) > 0) {
 					while($row = mysqli_fetch_object($result)) {
-						$datenuebergabe = [
-								[(float) $row -> humidity, 			//$row -> date_time,
+						$datenuebergabe[] = [
+								(float) $row -> humidity, 			//$row -> date_time,
 								(float) $row -> temperature,
-								(float) $row -> humidity],
+								(float) $row -> humidity,
 							];
 
 						echo  json_encode($datenuebergabe);
