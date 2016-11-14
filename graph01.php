5beb6e23b4e374a90b040ceff7a4b35b9ceaa175
kromar <grauer.d@gmail.com>
2016-11-14 16:02:39 +0100

    test

--- graph01.php
+++ graph01.php
@@ -39,10 +39,7 @@
 	google.load('visualization', '1', {	  packages: ['corechart', 'controls'] });
 	google.setOnLoadCallback(drawDashboard);
 
-
-
 	function drawDashboard() {
-
 	  var graphData = new google.visualization.DataTable();
 		graphData.addColumn('date', 'TIME'); //
 		graphData.addColumn('number', 'TEMP');
@@ -70,9 +67,21 @@
 				if (mysqli_num_rows($result) > 0) {
 					while($row = mysqli_fetch_object($result)) {
 						$datenuebergabe[] = [
-							new Date(2015, 12, 27, 23, 51, 21),
-							(float) $row -> temperature,
-							(float) $row -> humidity,
+								[new Date(2015, 0, 1), 5],  [new Date(2015, 0, 2), 7],  [new Date(2015, 0, 3), 3],
+								[new Date(2015, 0, 4), 1],  [new Date(2015, 0, 5), 3],  [new Date(2015, 0, 6), 4],
+								[new Date(2015, 0, 7), 3],  [new Date(2015, 0, 8), 4],  [new Date(2015, 0, 9), 2],
+								[new Date(2015, 0, 10), 5], [new Date(2015, 0, 11), 8], [new Date(2015, 0, 12), 6],
+								[new Date(2015, 0, 13), 3], [new Date(2015, 0, 14), 3], [new Date(2015, 0, 15), 5],
+								[new Date(2015, 0, 16), 7], [new Date(2015, 0, 17), 6], [new Date(2015, 0, 18), 6],
+								[new Date(2015, 0, 19), 3], [new Date(2015, 0, 20), 1], [new Date(2015, 0, 21), 2],
+								[new Date(2015, 0, 22), 4], [new Date(2015, 0, 23), 6], [new Date(2015, 0, 24), 5],
+								[new Date(2015, 0, 25), 9], [new Date(2015, 0, 26), 4], [new Date(2015, 0, 27), 9],
+								[new Date(2015, 0, 28), 8], [new Date(2015, 0, 29), 6], [new Date(2015, 0, 30), 4],
+								[new Date(2015, 0, 31), 6], [new Date(2015, 1, 1), 7],  [new Date(2015, 1, 2), 9]
+
+								//new Date(2015, 12, 27, 23, 51, 21),
+								//(float) $row -> temperature,
+								//(float) $row -> humidity,
 							];
 					}
 				} else {
