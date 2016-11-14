728097c0ef5469bb1a754b2d81cf7ffa77a66e4d
kromar <grauer.d@gmail.com>
2016-11-14 14:35:17 +0100

    change history size

--- graph01.php
+++ graph01.php
@@ -52,7 +52,7 @@
 				$password = "datalogger";
 				$dbname = "datalogger";
 				$datenuebergabe = array();
-				$history = 2;
+				$history = 10;
 				// Create connection
 				$db = mysqli_connect($servername, $username, $password, $dbname);
 				// Check connection
