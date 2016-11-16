fc26ea2ad3c0c716c5bb33083e0f39d023ccdfca
kromar <grauer.d@gmail.com>
2016-11-16 17:36:43 +0100

    fixed drop down for mobile layout

--- graph02.php
+++ graph02.php
@@ -43,12 +43,24 @@
 <body>
         <div id="wrapper">
 
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
 
                     <li>
-                         <a class="navbar-brand" href="index.php">Home</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Home</a>
                     </li>
 
                     <li class="active">
@@ -108,7 +120,13 @@
                 </ul>
             </div>
 
+            <!-- /.navbar-collapse -->
+        </nav>
 
+        <!-- viewport -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+            </div>
         </div>
         <!-- /#page-wrapper -->
 
