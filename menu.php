380234d1d8c20384843e875e3c0a47e728e07746
kromar <grauer.d@gmail.com>
2016-11-16 16:04:55 +0100

    separated htm part from php

--- menu.php
+++ menu.php
@@ -1,39 +1,4 @@
 <?php
-	echo '
-		<a href="/" tooltip title="HOME" alt="HOME">
-		<span class="fa-stack fa-2x">
-		  <i class="fa fa-circle fa-stack-2x"></i>
-		  <i class="fa fa-home fa-stack-1x fa-inverse"></i>
-		</span>
-		</a>
-		
-		<a href="/graph01.php" title="VIV 1" alt="VIV 1">
-		<span class="fa-stack fa-2x">
-		  <i class="fa fa-circle fa-stack-2x"></i>
-		  <strong class="fa-stack-1x fa-stack-text fa-inverse">1</strong>
-		</span>
-		</a>
-				
-		<a href="javascript:history.go(0);" title="RELOAD" alt="RELOAD">
-		<span class="fa-stack fa-2x">
-		  <i class="fa fa-circle fa-stack-2x"></i>
-		  <i class="fa fa-refresh fa-stack-1x fa-inverse"></i>
-		</span>
-		</a>
-		
-		<a href="/buttons.php" title="MANUAL CONTROLS" alt="MAN CTRLS">
-		<span class="fa-stack fa-2x">
-		  <i class="fa fa-circle fa-stack-2x"></i>
-		  <i class="fa fa-th fa-stack-1x fa-inverse"></i>
-		</span>
-		</a>
-		
-		<a href="/settings.php" title="SETTINGS/ABOUT" alt="SETTINGS">
-		<span class="fa-stack fa-2x">
-		  <i class="fa fa-circle fa-stack-2x"></i>
-		  <i class="fa fa-cog fa-stack-1x fa-inverse"></i>
-		</span>
-		</a>
-	';
+	include('menu.html');
 ?>
 
