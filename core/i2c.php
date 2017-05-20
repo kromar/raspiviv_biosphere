26b7823d4ee6d52ecce6d6a31df4233fe5a1c259
kromar <grauer.d@gmail.com>
2017-05-20 22:39:10 +0200

    reduced duration

--- core/i2c.php
+++ core/i2c.php
@@ -27,7 +27,7 @@
 				echo "pin: 0x0".dechex($pin)." mode:" .dechex($pin_mode)."\n";
 				system("i2cset -y 1 $PCF8574 0x0".dechex($pin));
 				$mode++;
-				usleep(500000);
+				usleep(100000);
 		 	}
 		 	sleep(10);
 
