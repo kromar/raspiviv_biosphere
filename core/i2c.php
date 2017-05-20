7f9c7c29af058a0d3fef35c51e65d98de3a97358
kromar <grauer.d@gmail.com>
2017-05-20 22:31:55 +0200

    loop duration

--- core/i2c.php
+++ core/i2c.php
@@ -27,7 +27,7 @@
 				echo "pin: 0x0".dechex($pin)." mode:" .dechex($pin_mode)."\n";
 				system("i2cset -y 1 $PCF8574 0x0".dechex($pin));
 				$mode++;
-				sleep(0.4);
+				sleep(0.8);
 		 	}
 		 	sleep(10);
 
