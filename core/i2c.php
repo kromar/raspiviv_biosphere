8a411f7f58035b67f444be61dc314eceef76a486
kromar <grauer.d@gmail.com>
2017-05-21 11:54:23 +0200

    added simulate function

--- core/i2c.php
+++ core/i2c.php
@@ -9,74 +9,63 @@
 	//i2cdetect -y 1
 	//i2cdump -y 0x..
 	//i2cset -y 1 0x27 0x..
+	//system("gpio mode $pin out");
+	//system("gpio write $pin $status[$pin]");
+	//exec ("gpio read ".$pin, $status[$pin], $return );
 
+	global $PCF8574, $pin_io, $statusActive;
 	$io_count = 8;
-	$pin_mode = 0;
 	$statusActive = False;
 	$PCF8574 = '0x27';
 
-	$pin = $argv[1];
-	$pin_state = $argv[2];
+	$statusActive = $argv[1];
+	$pin = $argv[2];
+	$pin_state = $argv[3];
+	$PCF8574 = $argv[4];
+	$pin_io = array(0,0,0,0,0,0,0,0);
 
 
-	convert($pin, $pin_state);
-
-	function convert($pin, $pin_state)
-	{
+	//this fucntion sets the pins of the ic to 1 or 0
+	function setICPins($pin, $pin_enabled) {
 		$pin = $pin-1;
-
-		$PCF8574 = '0x27';
-		$pin_io = array(0,0,0,0,0,0,0,0);
-
-
-		// now we want to convert the array to a hex output to write status
-
 		//set a specific output
-		if ($pin_state)
-		{
-			$pin_io[$pin] = $pin_state;
+		if ($pin_enabled) {
+			$pin_io[$pin] = $pin_enabled;
 		}
 
-		//echo our hex value from the binary array
+		//convert to hex value from the binary array
 		$binary = ($pin_io[0].$pin_io[1].$pin_io[2].$pin_io[3].$pin_io[4].$pin_io[5].$pin_io[6].$pin_io[7]);
-		$binary_pack = pack('H*', $binary);
 		$hex = "0x".dechex(bindec($binary));
-
 		echo "binary: ". $binary."\n";
 		echo "hex:". $hex."\n";
 		system("i2cset -y 1 $PCF8574 $hex");
-
 	}
 
-
-
-	while ($statusActive)
-	{
-
+// this function simulates switching through all io pins of the ic chip
+function simulateIO($statusActive) {
+	while ($statusActive) {
 		$mode = 0;
-		echo date('h:i:s'). " || ".$PCF8574. "\n";
-		system("i2cset -y 1 $PCF8574 0x00");
-		sleep(3);
-		system("i2cset -y 1 $PCF8574 0xF0");
-		sleep(3);
-		system("i2cset -y 1 $PCF8574 0x0F");
-		sleep(3);
-		system("i2cset -y 1 $PCF8574 0xFF");
-
-
-
-		for ($pin = 0; $pin < $io_count; $pin++)
-		 {
-				//system("gpio mode $pin out");
-				//system("gpio write $pin $status[$pin]");
-				echo "pin: 0x0".dechex($pin)."\n";
-				//system("i2cset -y 1 $PCF8574 0x0".dechex($pin));
-				$mode = $mode + 2;
+		// start enabling all pins
+		if ($mode == 0) {
+			for ($pin = 0; $pin < $io_count; $pin++)	 {
+				convert($pin, 1);
+				echo $pin;
+				$mode++;
 				usleep(200000);
-		 	}
-		 	sleep(3);
-
-			//exec ("gpio read ".$pin, $status[$pin], $return );
+			 	}
+			 	sleep(3);
+			}
+			//start disabling all pins
+		elseif ($mode==$io_count) {
+			for ($pin = $io_count; $pin > 0; $pin--) {
+				convert($pin, 0);
+				echo $pin;
+				$mode--;
+				usleep(200000);
+			 	}
+			 	sleep(10);
+			}
+		}
 	}
 ?>
 
