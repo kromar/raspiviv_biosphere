bab6c62c3e7416af14bf84bee9900a1e64663c0c
kromar <grauer.d@gmail.com>
2017-05-30 22:45:01 +0200

    added missing brackets

--- assets/js/input.js
+++ assets/js/input.js
@@ -2,22 +2,22 @@ $('#i2c_simulation').change(function() {
     //console.log(this);
     if(this.checked){       
         //Do stuff
-        $.ajax(
-            type: 'post',
+        $.ajax({
+            type: 'POST',
             url: 'core/i2c.php',             
             data: {simulate: 'True'},
             success: function(msg) {
                 alert("simulation:" + msg);
                 }
-        );
+        });
     } else {
-         $.ajax(
-            type: 'post',
+         $.ajax({
+            type: 'POST',
             url: 'core/i2c.php',
             data: {simulate: 'False'},
             success: function(msg) {
                 alert("simulation:" + msg);
                 }
-         );
+         });
     }
 });
