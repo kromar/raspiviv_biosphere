f46c7245d1b25e1d77233f64a4a5fb395a4875bd
kromar <grauer.d@gmail.com>
2017-05-31 20:54:31 +0200

    testing console log

--- core/i2c.php
+++ core/i2c.php
@@ -51,6 +51,18 @@
 	}
 
 
+
+function debug_to_console($data) {
+    if(is_array($data) || is_object($data))
+	{
+		echo("<script>console.log('PHP: ".json_encode($data)."');</script>");
+	} else {
+		echo("<script>console.log('PHP: ".$data."');</script>");
+	}
+}
+
+debug_to_console("file gets loaded");
+
 simulateIO($_POST['action']);
 
 // this function simulates switching through all io pins of the ic chip
