e4ea79374dae32b51023e32c7f6e240135fa15db
kromar <grauer.d@gmail.com>
2017-06-03 23:43:02 +0200

    restructuring

--- core/i2c.php
+++ core/i2c.php
@@ -32,54 +32,55 @@
 
 
 	//this fucntion sets the pins of the ic to 1 or 0
-	function setICPins($pin, $pin_enabled) {
+	function setICPins($pin, $pin_status) {
 
 		log_to_file("running function setICPins");
 
 		global $PCF8574, $pin_io;
+		log_to_file($pin);
 		$pin = $pin-1; 	//correction for physical pin vs array position
+
 		//set a specific output
 		if ($pin <= count($pin_io)){
-			$pin_io[$pin] = $pin_enabled;
+			$pin_io[$pin] = $pin_status;
+			//convert to hex value from the binary array
+			//$binary = ($pin_io[0].$pin_io[1].$pin_io[2].$pin_io[3].$pin_io[4].$pin_io[5].$pin_io[6].$pin_io[7]);
+			$binary = implode("", $pin_io);
+			$hex = "0x".dechex(bindec($binary));
+			//echo "binary: ". $binary."\n";
+			//echo "hex:". $hex."\n";
+			log_to_file("$pin i2cset -y 1 $PCF8574 $hex");
+			exec("i2cset -y 1 '0x27' $hex"); //or die(log_to_file("i2c failed"));
 		} else {
 			log_to_file("value out of array range");
 		}
-		//convert to hex value from the binary array
-		//$binary = ($pin_io[0].$pin_io[1].$pin_io[2].$pin_io[3].$pin_io[4].$pin_io[5].$pin_io[6].$pin_io[7]);
-		$binary = implode("", $pin_io);
-		$hex = "0x".dechex(bindec($binary));
-		//echo "binary: ". $binary."\n";
-		//echo "hex:". $hex."\n";
-		log_to_file("$pin i2cset -y 1 $PCF8574 $hex");
-		exec("i2cset -y 1 '0x27' $hex"); //or die(log_to_file("i2c failed"));
 	}
-
 
 	$simulationActive = $_POST['action'];
 
 	simulateIO($simulationActive);
 	// this function simulates switching through all io pins of the ic chip
 	function simulateIO($simulationActive) {
-		$mode = 1;
+		$direction = 1;		 //1=up; 0=down
 		$io_count = 8;
 		log_to_file($simulationActive);
 		//while ($simulationActive == True) {
 			// start enabling all pins
-			if ($mode == 1) {
+			if ($direction == 1) {
 				for ($pin = 1; $pin <= $io_count; $pin++)	 {
 					setICPins($pin, 1);
 					exec(usleep(200000));
 			 	}
-			 	$mode = 0;
+			 	$direction = 0;
 			 	exec(sleep(1));
 			}
 			//start disabling all pins
-			elseif ($mode== 0) {
+			elseif ($direction== 0) {
 				for ($pin = $io_count; $pin >= 1; $pin--) {
 					setICPins($pin, 0);
 					exec(usleep(200000));
 			 	}
-			 	$mode = 1;
+			 	$direction = 1;
 			 	exec(sleep(3));
 			}
 		//}
