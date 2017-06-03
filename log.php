e8b304586891873a86db1c216755a5ae7e1bcea8
kromar <grauer.d@gmail.com>
2017-06-03 13:57:14 +0200

    optimizing log to file function

--- log.php
+++ log.php
@@ -1,23 +1,23 @@
 <?php
 	function log_to_file($value0, $value1, $value2) {
 		$megabytes = 2;
-		$file = "../../log/raspiviv.log";
-		$size = filesize(__DIR__.$file);
+		$file = "/../../log/raspiviv.log";
+		$logfile = fopen(__DIR__.$file, "a+") or die("Unable to open file!");
+		$size = filesize($logfile);
 		$curentTime = date('H:i:s');
+
 		if ($size < $megabytes * 1024 * 1000) {
-			$mylogfile = fopen(__DIR__.$file, "a") or die("Unable to open file!");
 			try {
 				//fwrite($mylogfile, $curentTime . " size: " . $size ." file: " . __DIR__ . $file ."\n");
-				fwrite($mylogfile, "<b> $curentTime  $value0  $value1  $value2 </b> \n");
-				fclose($mylogfile);
+				fwrite($logfile, "<b> $curentTime  $value0  $value1  $value2 </b> \n");
+				fclose($logfile);
 
 			} catch (Exception $e) {
 				echo 'Caught exception: ',  $e->getMessage(), "\n";
 			}
 		} else {
-			$mylogfile = fopen(__DIR__ . $file, "w") or die("Unable to open file!");
-			fwrite($mylogfile, $curentTime . " reset file size \n");
-			fclose($mylogfile);
+			fwrite($logfile, $curentTime . " reset file size \n");
+			fclose($logfile);
 		}
 	}
 
