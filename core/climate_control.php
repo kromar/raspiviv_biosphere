7014a6529700f2652f0d742d50c35f21e09610e3
kromar <grauer.d@gmail.com>
2018-01-18 20:17:05 +0100

    reduced misting cycle

--- core/climate_control.php
+++ core/climate_control.php
@@ -22,7 +22,7 @@
 
 	// different ways of shedule input (fixed time, intervals with exceptions (night), dynamic(sensor based))
 	//$rainShedule = array('12:00' => 20, '18:00' => 10);
-	$rainShedule = array('12:00' => 10, '14:00' => 5, '16:00' => 7, '18:00' => 5);
+	$rainShedule = array('12:00' => 10, '16:00' => 5);
 
 	$rainTime = 1; 			// time in seconds to rain
 	$windTime = 10;			// time to vent in seconds
