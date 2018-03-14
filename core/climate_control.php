2b944a122ef5cb8b979586d3016068b8263be5d0
kromar <grauer.d@gmail.com>
2018-03-14 18:23:46 +0100

    enabled summer profile for low humidity rain

--- core/climate_control.php
+++ core/climate_control.php
@@ -10,9 +10,9 @@
 	$humidityNight = 85.0;
 	$humidityDay = 80.0;
 	global $highTempRain, $humidityMin;
-	$humidityMin = 65.0;
+	$humidityMin = 55.0;
 	$highTempRain = false;
-	$lowHumRain = false;
+	$lowHumRain = true;
 	global $currentTime, $sunriseTime, $sunsetTime;
 	$currentTime = date('H:i');
 	$sunriseTime = ('10:00');
