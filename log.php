0b96541eb0f0ca35fa506b7ae27edaa6c1d3e61d
kromar <grauer.d@gmail.com>
2018-03-25 22:20:35 +0200

    fix file truncation

--- log.php
+++ log.php
@@ -7,9 +7,10 @@
 		if (file_exists($path.$file)) {
 			$size = filesize($path.$file);
 			$curentTime = date('H:i:s');
+
 			//reset file size if it grows to big
 			if ($size < $megabytes * 1024 * 1000) {
-				$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
+				$mylogfile = fopen($path.$file, "a") or die("Unable to open file!"); 		// Open for writing only; place the file pointer at the end of the file. If the file does not exist, attempt to create it.
 				fwrite($mylogfile, "<b> $curentTime  $value</b> \n");
 				fclose($mylogfile);
 			} else {
