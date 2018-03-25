6b3595481a18c0a4247f0d1a581f112e22625225
kromar <grauer.d@gmail.com>
2018-03-25 21:53:41 +0200

    fix for file and path creation

--- log.php
+++ log.php
@@ -4,32 +4,31 @@
 		$path ='/var/log/raspiviv/';
 		$file = "raspiviv.log";
 
-		if (file_exists($path.$file)) {
-			$size = filesize($path.$file);
-			$curentTime = date('H:i:s');
+		if (file_exists($path)) {
+			if (file_exists($path.$file)) {
+				$size = filesize($path.$file);
+				$curentTime = date('H:i:s');
 
-			//reset file size if it grows to big
-			if ($size < $megabytes * 1024 * 1000) {
-				$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
-				try {
-					fwrite($mylogfile, "<b> $curentTime  $value0  $value1  $value2 </b> \n");
-					fclose($mylogfile);
+				//reset file size if it grows to big
+				if ($size < $megabytes * 1024 * 1000) {
+					$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
+					try {
+						fwrite($mylogfile, "<b> $curentTime  $value0  $value1  $value2 </b> \n");
+						fclose($mylogfile);
 
-				} catch (Exception $e) {
-					echo 'Caught exception: ',  $e->getMessage(), "\n";
+					} catch (Exception $e) {
+						echo 'Caught exception: ',  $e->getMessage(), "\n";
+					}
+				}
+			} else {
 					fwrite($mylogfile, $curentTime . " reset file size \n");
 					fclose($mylogfile);
-				}
 			}
 		} else {
-			try {
-				mkdir($path, 0777, true);
-			} catch (Exception $e) {
-				echo 'Caught exception: ',  $e->getMessage(), "\n";
-				$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
-				fwrite($mylogfile, $curentTime . " reset file size \n");
-				fclose($mylogfile);
-			}
+			mkdir($path, 0777, true);
+			$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
+			fwrite($mylogfile, $curentTime . " reset file size \n");
+			fclose($mylogfile);
 		}
 	}
 ?>
