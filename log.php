703a008dd61ac96249b52eade04090bcddfa6fde
kromar <grauer.d@gmail.com>
2018-03-25 22:16:13 +0200

    fixed file truncate

--- log.php
+++ log.php
@@ -4,16 +4,14 @@
 		$path ='/var/log/raspiviv/';
 		$file = "raspiviv.log";
 
-		if (file_exists($path)) {
-			if (file_exists($path.$file)) {
-				$size = filesize($path.$file);
-				$curentTime = date('H:i:s');
-				//reset file size if it grows to big
-				if ($size < $megabytes * 1024 * 1000) {
-					$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
-					fwrite($mylogfile, "<b> $curentTime  $value</b> \n");
-					fclose($mylogfile);
-				}
+		if (file_exists($path.$file)) {
+			$size = filesize($path.$file);
+			$curentTime = date('H:i:s');
+			//reset file size if it grows to big
+			if ($size < $megabytes * 1024 * 1000) {
+				$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
+				fwrite($mylogfile, "<b> $curentTime  $value</b> \n");
+				fclose($mylogfile);
 			} else {
 				$mylogfile = fopen($path.$file, "w") or die("Unable to open file!");
 				fwrite($mylogfile, $curentTime . " reset file size \n");
