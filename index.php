450ebc724b856bb093bd0a2f142e5670d7e77af2
kromar <grauer.d@gmail.com>
2018-07-01 17:38:37 +0200

    fix css links

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 	<title>RasPiViv.com - Home</title>
 
-    <!--Load the AJAX API-->
+     <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script type="text/javascript"></script>
@@ -25,9 +25,10 @@
 	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-   	<link rel="stylesheet" href="http://getbootstrap.com/examples/cover/cover.css">
+<!--    	<link rel="stylesheet" href="http://getbootstrap.com/examples/cover/cover.css"> -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-
+	<script src="//cdnjs.cloudflare.com/ajax/libs/dygraph/1.1.1/dygraph-combined.js"></script>
+</head>
 
 	<!-- ROOM TEMP GAUGE -->
 	<script type="text/javascript">
